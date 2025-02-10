ecbab61689747a1f9f2b6485af04d9b45892d8d2
Bhushan <bhushannalawade318@gmail.com>
2025-02-10 14:25:29 +0530

    object added

--- git_practice/1.js
+++ git_practice/1.js
@@ -1,1 +1,36 @@
 console.log("hello world");
+
+const arr = [
+    {
+        name:"abc",
+        roll:123,
+    },
+    {
+        name:"def",
+        roll:456,
+    },
+    {
+        name:"ghi",
+        roll:789,
+    },
+    {
+        name:"jkl",
+        roll:10,
+    },
+    {
+        name:"mno",
+        roll:12,
+    },
+    {
+        name:"pqr",
+        roll:13,
+    }
+    
+]
+
+
+arr.forEach(data => {
+    // console.log(data.name);
+    console.log(data.roll);
+    
+});
